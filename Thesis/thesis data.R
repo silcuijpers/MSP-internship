a98878f19be9f893f7a91f205243596b356ca871
Coort, Susan (BIGCAT) <susan.coort@maastrichtuniversity.nl>
2024-03-06 21:27:57 +0100

    Update thesis data.R

--- Thesis/thesis data.R
+++ Thesis/thesis data.R
@@ -1,4 +1,23 @@
+#clean workspace TEST
 rm(list=ls())
+
+########################################################################################
+## You can add the code below to install the R-packages if you don't have them yet
+## this code is for packages that are originating from Bioconductor
+
+# Install BiocManager from CRAN
+install.packages("BiocManager")
+
+# Load BiocManager
+library(BiocManager)
+
+# Install Bioconductor packages
+BiocManager::install("GEOquery")
+BiocManager::install("limma")
+
+######################################################################################## 
+
+#load installed packages
 library(GEOquery)
 library(limma)
 
@@ -7,11 +26,13 @@ gset <- getGEO("GSE28358",GSEMatrix=TRUE, AnnotGPL=TRUE)
 if (length(gset) > 1) idx <- grep("GPL571", attr(gset, "names")) else idx <- 1
 gset <- gset[[idx]]
 
-# make column names
+# Make column names by using the sample names
+# In the current dataset this is for example "GSM701076"
 fvarLabels(gset) <- make.names(fvarLabels(gset))
 
 
-#assigning eacht sample to one of the six groups
+# assigning each sample to one of the six groups
+# Did you check whether this maps with the assignement of the groups to the samples? 
 gsms <- "10101010101010101032323232323232323232544441054545454554545410324"
 sml <- strsplit(gsms, split="")[[1]]
 
@@ -35,10 +56,10 @@ gset <- gset[complete.cases(exprs(gset)), ]
 fit <- lmFit(gset, design) 
 
 #making a contrast matrix of group1 olive oil and group2 baseline to calculate the log 2 fold change 
-cts.nuts <- c(paste(groups[1],"-",groups[2],sep=""))
+cts.olive <- c(paste(groups[1],"-",groups[2],sep=""))
 cont.matrix.olive<- makeContrasts(contrasts=cts.olive, levels=design)
 fit.olive <- contrasts.fit(fit, cont.matrix.olive)
-fit.olive <- eBayes(fit.olive, 0.01)# standard devitaion of 0.01
+fit.olive <- eBayes(fit.olive, 0.01)# standard deviation of 0.01
 
 cts.nuts <- c(paste(groups[3],"-",groups[4],sep=""))
 cont.matrix.nuts<- makeContrasts(contrasts=cts.nuts, levels=design)
@@ -55,15 +76,37 @@ tT.olive<- topTable(fit.olive, adjust="BH", sort.by="B", number=Inf)
 tT.nuts<-topTable(fit.nuts, adjust="BH", sort.by="B", number = Inf)
 tT.lowfat<-topTable(fit.lowfat, adjust="BH", sort.by="B", number= Inf)
 
-
+# It think you tried to map the Affymetrix IDs with the Gene Symbols in the expression set,
+# but this won't work. 
 tT.olive <- subset(tT.olive, select=c("ID","adj.P.Val","P.Value","F","GB_ACC","SPOT_ID","Gene.Symbol","Gene.symbol","Gene.title"))
 write.table(tT.olive, file=stdout(), row.names=F, sep="\t")
 
-
-tT.olive <- topTable(fit.olive, adjust="BH", sort.by="B", number=Inf)
+# Olive oil: This is a plot of the adjusted p-value
+# I would do the same to plot the p-value distribution. 
 hist(tT.olive$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
-     ylab = "Number of genes", main = "P-adj value distribution")
+     ylab = "Number of genes", main = "Olive oil: P-adj value distribution")
 
-tT.nuts<- topTable(fit.nuts, adjust="BH", sort.by="B", number=Inf)
+# Olive oil p-value distribution
+hist(tT.olive$P.Value, col = "grey", border = "white", xlab = "P.Value",
+     ylab = "Number of genes", main = "Olive oil: P.Value distribution")
+
+# Nuts adjusted p-value
 hist(tT.nuts$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
-     ylab = "Number of genes", main = "P-adj value distribution")
+     ylab = "Number of genes", main = "Nuts: P-adj value distribution")
+
+# Nuts p-value distribution
+hist(tT.nuts$P.Value, col = "grey", border = "white", xlab = "P.Value",
+     ylab = "Number of genes", main = "Nuts: P.Value distribution")
+
+# Lowfat adjusted p-value
+hist(tT.lowfat$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+     ylab = "Number of genes", main = "Low fat: P-adj value distribution")
+
+# Nuts p-value distribution
+hist(tT.lowfat$P.Value, col = "grey", border = "white", xlab = "P.Value",
+     ylab = "Number of genes", main = "Low fat: P.Value distribution")
+
+### Determine the differentially expressed genes (DEGs)
+
+### Create a Volcano plot 
+
