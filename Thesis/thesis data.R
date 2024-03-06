75b474192637e1f997265f796dc0b45bfc650cfa
silcuijpers <159008011+silcuijpers@users.noreply.github.com>
2024-03-06 10:10:44 +0100

    Update thesis data.R

--- Thesis/thesis data.R
+++ Thesis/thesis data.R
@@ -22,7 +22,7 @@ exprs(gset) <- log2(ex)
 
 # assigning a groupname to each sample
 gs <- factor(sml)
-groups <- make.names(c("olive oil","baseline","nuts","nuts baseline","low list","low fat baseline"))
+groups <- make.names(c("olive oil","baseline","nuts","nuts baseline","low fat","low fat baseline"))
 levels(gs) <- groups
 gset$group <- gs
 design <- model.matrix(~group + 0, gset)
@@ -34,22 +34,36 @@ gset <- gset[complete.cases(exprs(gset)), ]
 #fitting a linear model to gene expression data
 fit <- lmFit(gset, design) 
 
-#making a contrast matrix of group1 olive oil and group2 baseline to calculate the log 2 gold change and make a top tabble ranked on B-> Log2fold change
-cts.olive <- c(paste(groups[1],"-",groups[2],sep=""))
+#making a contrast matrix of group1 olive oil and group2 baseline to calculate the log 2 fold change 
+cts.nuts <- c(paste(groups[1],"-",groups[2],sep=""))
 cont.matrix.olive<- makeContrasts(contrasts=cts.olive, levels=design)
-fit.olive <- contrasts.fit(fit, cont.matrix)
+fit.olive <- contrasts.fit(fit, cont.matrix.olive)
 fit.olive <- eBayes(fit.olive, 0.01)# standard devitaion of 0.01
-tT.olive<- topTable(fit.olive, adjust="fdr", sort.by="B", number=Inf)
+
+cts.nuts <- c(paste(groups[3],"-",groups[4],sep=""))
+cont.matrix.nuts<- makeContrasts(contrasts=cts.nuts, levels=design)
+fit.nuts <- contrasts.fit(fit, cont.matrix.nuts)
+fit.nuts <- eBayes(fit.nuts, 0.01)
+
+cts.lowfat <- c(paste(groups[5],"-",groups[6],sep=""))
+cont.matrix.lowfat<- makeContrasts(contrasts=cts.lowfat, levels=design)
+fit.lowfat <- contrasts.fit(fit, cont.matrix.lowfat)
+fit.lowfat <- eBayes(fit.lowfat, 0.01)
+
+#making top tables for each group using benjamini-hochberg to adjust p-values
+tT.olive<- topTable(fit.olive, adjust="BH", sort.by="B", number=Inf)
+tT.nuts<-topTable(fit.nuts, adjust="BH", sort.by="B", number = Inf)
+tT.lowfat<-topTable(fit.lowfat, adjust="BH", sort.by="B", number= Inf)
 
 
+tT.olive <- subset(tT.olive, select=c("ID","adj.P.Val","P.Value","F","GB_ACC","SPOT_ID","Gene.Symbol","Gene.symbol","Gene.title"))
+write.table(tT.olive, file=stdout(), row.names=F, sep="\t")
 
 
+tT.olive <- topTable(fit.olive, adjust="BH", sort.by="B", number=Inf)
+hist(tT.olive$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+     ylab = "Number of genes", main = "P-adj value distribution")
 
-tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","F","GB_ACC","SPOT_ID","Gene.Symbol","Gene.symbol","Gene.title"))
-write.table(tT, file=stdout(), row.names=F, sep="\t")
-
-
-
-tT2 <- topTable(fit2, adjust="fdr", sort.by="B", number=Inf)
-hist(tT2$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+tT.nuts<- topTable(fit.nuts, adjust="BH", sort.by="B", number=Inf)
+hist(tT.nuts$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
      ylab = "Number of genes", main = "P-adj value distribution")
